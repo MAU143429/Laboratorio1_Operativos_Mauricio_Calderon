a2512473daa7f4920c77ee710e66be9d288ebe9c
MAU143429 <mau141226@estudiantec.cr>
2024-03-18 13:46:18 -0600

    Update factorial.c
    Se actualizo factorial.c para poder adaptarlo a valores de mas de 100 factorial sin que exista desbordamiento

--- factorial.c
+++ factorial.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 
-long long int factorial(long long int n) {
-    // Factorial de 0
+double factorial(int n) {
     if (n == 0) {
         return 1;
-    }
-    // Factorial de n-1
-    else {
-        return n * factorial(n-1);
+    } else {
+        return n * factorial(n - 1);
     }
 }
 
 int main() {
-    long long int numero = 30;
+    int numero = 110;
     
     clock_t inicio = clock();  
     
-    long long int resultado = factorial(numero);
+    double resultado = factorial(numero);
     
     clock_t fin = clock();  
 
-    printf("El factorial de %lld es %lld\n", numero, resultado);
+    printf("El factorial de %d es %.0lf\n", numero, resultado);
     printf("Tiempo de ejecución: %f segundos\n", (double)(fin - inicio) / CLOCKS_PER_SEC);
 
     return 0;
